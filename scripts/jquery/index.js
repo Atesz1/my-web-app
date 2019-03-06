05085461cbbaa845e720fe3213af81fdca333986
BambuszMahmud <attilabecsei@gmail.com>
2019-03-06 04:40:50 +0100

    asdasd

--- scripts/jquery/index.js
+++ scripts/jquery/index.js
@@ -1,9 +1,49 @@
-$(window).resize(logScreenSize);
+// $(window).resize(logScreenSize);
 
 
 $(function(){
-  logScreenSize();
+  // logScreenSize();
+  resizeElements(['.my-button-people-name','.my-button-jump-to-people']);
 });
+
+
+//function resizeButtons(){
+  //var width = 0;
+
+  //$('.my-button-people-name').css('width', 'auto');
+
+  //$('.my-button-people-name').each(function(){
+      //console.log($(this).css('width'));
+    //var thiswidth = parseFloat($(this).css('width'));
+
+    //if(thiswidth > width){
+      //width = thiswidth;
+    //}
+
+  //});
+
+  //$('.my-button-people-name').css('width', width);
+//}
+
+function resizeElements(elements){
+  $.each(elements, function(index, element)){
+
+  var width = 0;
+
+
+  $(element).css('width', 'auto');
+
+  $(element).each(function(){
+      //console.log($(this).css('width'));
+    var thiswidth = parseFloat($(this).css('width'));
+
+    if(thiswidth > width){
+      width = thiswidth;
+    }
+  });
+
+  $(element).css('width', width);
+}
 
 
 function logScreenSize(){

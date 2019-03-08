9e982331f7c10ab9c6ed0c781c96e42fc88845c9
BambuszMahmud <attilabecsei@gmail.com>
2019-03-08 04:47:38 +0100

    8/3/19

--- scripts/jquery/index.js
+++ scripts/jquery/index.js
@@ -3,4 +3,26 @@
 $(function(){
   // logScreenSize();
   resizeElements(['.my-button-people-name', '.my-button-jump-to-people'])
+  if (Modernizr.mq)('(min-width:576px)'){
+    $('.my-button-people-name').css('width','180px');
+
+  }
+
+
+  $('.my-button-people-name').on('click', function ())
+    $(this).toggleClass('btn-secondary btn-danger');
+var $button= $(this).closest('div').find('.my-button-jump-to-people').toggleClass('d-none d-block');
+      $button.toggleClass=('d-none');
+  var id = $button.find('a').attr('href');
+          $(id).toggleClass('d-none');
+  }
+
 });
+
+
+.my-flexbox-container{
+  display:flex;
+  .flex-flow: row wrap;
+  allign-contetnt:flex-start;
+  justify-content: space-between;
+}
